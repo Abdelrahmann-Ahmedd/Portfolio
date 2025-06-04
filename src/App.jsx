0b274963b7df1323df79f523ebf6efacbdeb41e1
Abdelrahmann-Ahmedd <abdelrahman.w164@gmail.com>
2025-06-04 04:58:32 +0300

    Add routing for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
+import Layout from './Pages/Layout/Layout'
+import Portfolio from './Pages/Portfolio/Portfolio';
+import Contact from './Pages/Contact/Contact';
+import Resume from './Pages/Resume/Resume';
+import About from './Pages/About/About';
+
+const myRouter = createBrowserRouter([
+  {path:"/" , element: <Layout/> ,children: [
+    { path:"/", element: <Portfolio/>},
+    { path:"/portfolio", element: <Portfolio/>},
+    { path:"/contact", element: <Contact/>},
+    { path:"/resume", element: <Resume/>},
+    { path:"/About", element: <About/>},
+    { path:"/About", element: <About/>},
+    { path:"*", element: <Error/>},
+  ]}
+])
 
 function App() {
 
   return (
     <>
+      <RouterProvider router={myRouter} />
     </>
   )
 }
